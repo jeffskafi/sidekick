eb9e1ed81230c479e019f8e14e964920d8ab2def
jaafar skafi <jaafarskafi1@gmail.com>
2024-07-15 14:12:08 -0700

    lint

--- src/_components/AgentInfoDrawer.tsx
+++ src/_components/AgentInfoDrawer.tsx
@@ -1,17 +1,19 @@
 import { Drawer, DrawerContent, DrawerTrigger } from "~/components/ui/drawer";
 import dynamic from "next/dynamic";
 
-const DynamicAgentDrawer = dynamic(() => import("./AgentDrawer"), { ssr: false });
+const DynamicAgentDrawer = dynamic(() => import("./AgentDrawer"), {
+  ssr: false,
+});
 
 export function AgentInfoDrawer() {
   return (
     <Drawer>
       <DrawerTrigger asChild>
-        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
+        <button className="mt-4 rounded bg-blue-500 px-4 py-2 text-white">
           Show Agent Data
         </button>
       </DrawerTrigger>
-      <DrawerContent className="w-1/3 max-w-md mx-auto left-0 right-0">
+      <DrawerContent className="left-0 right-0 mx-auto w-1/3 max-w-md">
         <DynamicAgentDrawer />
       </DrawerContent>
     </Drawer>
