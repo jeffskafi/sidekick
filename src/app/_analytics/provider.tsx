8377103325368f1e2228571b6e0ac44fd69a2864
jaafar skafi <jaafarskafi1@gmail.com>
2024-08-03 08:35:05 -0700

    configure reverse proxy for Posthog

--- src/app/_analytics/provider.tsx
+++ src/app/_analytics/provider.tsx
@@ -6,7 +6,8 @@ import { useEffect } from "react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_API_HOST,
+    ui_host: process.env.NEXT_PUBLIC_POSTHOG_UI_HOST,
   });
 }
 
