4ad6f26d42669d816e4442b5c1e12a3a03010a55
jaafar skafi <jaafarskafi1@gmail.com>
2024-08-11 14:02:10 -0700

    remove openai logic from main middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,18 +12,12 @@ export default clerkMiddleware((auth, req) => {
   // Check if the user is authenticated
   const { userId } = auth();
 
-// If the user is not authenticated and trying to access a protected route
-if (!userId) {
-  const signInUrl = new URL("/sign-in", req.url);
-  signInUrl.searchParams.set("redirect_url", req.url);
-
-  // Check if the request is from OpenAI and set the redirect URL accordingly
-  if (req.headers.get("referer")?.includes("openai.com")) {
-    signInUrl.href = process.env.OPENAI_CHATGPT_REDIRECT_URL!;
+  // If the user is not authenticated and trying to access a protected route
+  if (!userId) {
+    const signInUrl = new URL("/sign-in", req.url);
+    signInUrl.searchParams.set("redirect_url", req.url);
+    return NextResponse.redirect(signInUrl);
   }
-
-  return NextResponse.redirect(signInUrl);
-}
 
   // Allow authenticated users to access protected routes
   return NextResponse.next();
