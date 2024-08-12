77232b6e298a7c1eb5668dd2f61041e504a1535b
jaafar skafi <jaafarskafi1@gmail.com>
2024-08-12 16:38:40 -0700

    whitelist clerk oauth

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export default clerkMiddleware((auth, req) => {
-  const publicPaths = ["/", "/sign-in", "/sign-up", "/privacy"];
+  const publicPaths = ["/", "/sign-in", "/sign-up", "/privacy", "/api/auth/callback/clerk"];
   const isPublicPath = publicPaths.includes(req.nextUrl.pathname);
 
   if (isPublicPath) {
