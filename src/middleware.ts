740d5e525534c569c14beddea0b8fc9e9e9e76d3
jaafar skafi <jaafarskafi1@gmail.com>
2024-08-13 01:08:32 -0700

    remove all logs and restore code

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,41 +2,20 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export default clerkMiddleware((auth, req) => {
-  console.log("auth", auth);
   const publicPaths = ["/", "/sign-in", "/sign-up", "/privacy"];
   const isPublicPath = publicPaths.includes(req.nextUrl.pathname);
-  console.log("isPublicPath", isPublicPath);
-  console.log("req.nextUrl.pathname", req.nextUrl.pathname);
-  console.log("req.url", req.url);
-  console.log("req.method", req.method);
-  console.log("req.headers", req.headers);
-  console.log("req.body", req.body);
-  console.log("req.cookies", req.cookies);
-  console.log("req.nextUrl", req.nextUrl);
-  console.log("req.nextUrl.origin", req.nextUrl.origin);
-  console.log("req.nextUrl.pathname", req.nextUrl.pathname);
-  console.log("req.nextUrl.searchParams", req.nextUrl.searchParams);
 
   if (isPublicPath) {
     return NextResponse.next();
   }
 
   // Check if the user is authenticated
-  const authObject = auth();
-  console.log("authObject", authObject);
-  const userId = authObject.userId;
-  console.log("userId", userId);
-  authObject
-    .getToken()
-    .then((token) => console.log("TOKEN", token))
-    .catch((error) => console.log("ERROR", error));
+  const { userId } = auth();
 
   // If the user is not authenticated and trying to access a protected route
   if (!userId) {
     const signInUrl = new URL("/sign-in", req.url);
-    console.log("signInUrl", signInUrl);
     signInUrl.searchParams.set("redirect_url", req.url);
-    console.log("signInUrl", signInUrl);
     return NextResponse.redirect(signInUrl);
   }
 
