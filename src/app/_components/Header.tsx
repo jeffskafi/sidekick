813947a38813598087601041e5ad196520b00aea
jaafar skafi <jaafarskafi1@gmail.com>
2024-08-30 17:53:00 -0700

    use feature flag

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -22,11 +22,11 @@ const Header = () => {
   const { isSignedIn } = useAuth();
   const { isDarkMode } = useDarkMode();
   const subscriptionsFlagEnabled = useFeatureFlagEnabled("subscriptions");
-  const mindMapFlagEnabled = true;
+  const mindMapFlagEnabled = useFeatureFlagEnabled("mind-map");
   const pathname = usePathname();
 
   return (
-    <header className="sticky top-0 z-50 bg-white dark:bg-dark-bg shadow-sm">
+    <header className="sticky top-0 z-50 bg-white shadow-sm dark:bg-dark-bg">
       <nav className="container mx-auto px-4 py-2">
         <div className="flex flex-col space-y-2 sm:space-y-0">
           <div className="flex items-center justify-between">
@@ -35,7 +35,8 @@ const Header = () => {
                 <h1
                   className={`text-[2rem] font-bold ${isDarkMode ? "text-white" : "text-black"}`}
                   style={{
-                    background: "linear-gradient(90deg, #ff7247 0%, #e63b00 100%)",
+                    background:
+                      "linear-gradient(90deg, #ff7247 0%, #e63b00 100%)",
                     WebkitBackgroundClip: "text",
                     WebkitTextFillColor: "transparent",
                     filter: "drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.3))",
@@ -44,14 +45,14 @@ const Header = () => {
                   Sidekick
                 </h1>
               </Link>
-              <div className="hidden sm:flex space-x-4">
+              <div className="hidden space-x-4 sm:flex">
                 {isSignedIn && (
                   <>
                     <Link
                       href="/tasks"
                       className={`text-sm font-medium transition-colors duration-200 ${
                         pathname === "/tasks"
-                          ? "text-amber-500 border-b-2 border-amber-500"
+                          ? "border-b-2 border-amber-500 text-amber-500"
                           : "text-gray-600 hover:text-amber-500 dark:text-gray-300 dark:hover:text-amber-400"
                       }`}
                     >
@@ -62,7 +63,7 @@ const Header = () => {
                         href="/mind-map"
                         className={`text-sm font-medium transition-colors duration-200 ${
                           pathname === "/mind-map"
-                            ? "text-amber-500 border-b-2 border-amber-500"
+                            ? "border-b-2 border-amber-500 text-amber-500"
                             : "text-gray-600 hover:text-amber-500 dark:text-gray-300 dark:hover:text-amber-400"
                         }`}
                       >
@@ -73,7 +74,7 @@ const Header = () => {
                       href="https://chatgpt.com/g/g-DMgdGagJK-sidekick"
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="text-sm font-medium transition-colors duration-200 text-gray-600 hover:text-amber-500 dark:text-gray-300 dark:hover:text-amber-400 flex items-center"
+                      className="flex items-center text-sm font-medium text-gray-600 transition-colors duration-200 hover:text-amber-500 dark:text-gray-300 dark:hover:text-amber-400"
                     >
                       ChatGPT
                       <ExternalLink size={14} className="ml-1" />
@@ -84,30 +85,30 @@ const Header = () => {
             </div>
             {isSignedIn ? (
               <UserButton
-              afterSignOutUrl="/"
-              appearance={{
-                elements: {
-                  avatarBox: "w-10 h-10",
-                },
-              }}
-            >
-              <UserButton.UserProfilePage
-                label="Appearance"
-                url="appearance"
-                labelIcon={<Palette size={16} />}
+                afterSignOutUrl="/"
+                appearance={{
+                  elements: {
+                    avatarBox: "w-10 h-10",
+                  },
+                }}
               >
-                <AppearancePage />
-              </UserButton.UserProfilePage>
-              {subscriptionsFlagEnabled && (
                 <UserButton.UserProfilePage
-                  label="Subscription"
-                  url="subscription"
-                  labelIcon={<CreditCard size={16} />}
+                  label="Appearance"
+                  url="appearance"
+                  labelIcon={<Palette size={16} />}
                 >
-                  <SubscriptionPage />
+                  <AppearancePage />
                 </UserButton.UserProfilePage>
-              )}
-            </UserButton>
+                {subscriptionsFlagEnabled && (
+                  <UserButton.UserProfilePage
+                    label="Subscription"
+                    url="subscription"
+                    labelIcon={<CreditCard size={16} />}
+                  >
+                    <SubscriptionPage />
+                  </UserButton.UserProfilePage>
+                )}
+              </UserButton>
             ) : (
               <SignInButton mode="modal">
                 <button className="rounded-full bg-primary-light px-4 py-2 text-sm font-medium text-white transition-colors duration-300 hover:bg-secondary-light dark:bg-primary-dark dark:hover:bg-secondary-dark">
@@ -116,14 +117,14 @@ const Header = () => {
               </SignInButton>
             )}
           </div>
-          <div className="sm:hidden flex space-x-4">
+          <div className="flex space-x-4 sm:hidden">
             {isSignedIn && (
               <>
                 <Link
                   href="/tasks"
                   className={`text-sm font-medium transition-colors duration-200 ${
                     pathname === "/tasks"
-                      ? "text-amber-500 border-b-2 border-amber-500"
+                      ? "border-b-2 border-amber-500 text-amber-500"
                       : "text-gray-600 hover:text-amber-500 dark:text-gray-300 dark:hover:text-amber-400"
                   }`}
                 >
@@ -134,7 +135,7 @@ const Header = () => {
                     href="/mind-map"
                     className={`text-sm font-medium transition-colors duration-200 ${
                       pathname === "/mind-map"
-                        ? "text-amber-500 border-b-2 border-amber-500"
+                        ? "border-b-2 border-amber-500 text-amber-500"
                         : "text-gray-600 hover:text-amber-500 dark:text-gray-300 dark:hover:text-amber-400"
                     }`}
                   >
@@ -145,7 +146,7 @@ const Header = () => {
                   href="https://chatgpt.com/g/g-DMgdGagJK-sidekick"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-sm font-medium transition-colors duration-200 text-gray-600 hover:text-amber-500 dark:text-gray-300 dark:hover:text-amber-400 flex items-center"
+                  className="flex items-center text-sm font-medium text-gray-600 transition-colors duration-200 hover:text-amber-500 dark:text-gray-300 dark:hover:text-amber-400"
                 >
                   ChatGPT
                   <ExternalLink size={14} className="ml-1" />
